7c9f7e61d8e25407fab6fb04f07b32dbca7becd7
Lyudmila <milo4kamoya@gmail.com>
2022-09-21 13:04:03 -0700

    Reducer Setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-const container = document.getElementById('root')
-
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>, 
