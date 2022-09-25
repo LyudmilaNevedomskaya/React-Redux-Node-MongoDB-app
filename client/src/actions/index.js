c8bbd4d387f780aca6821d52e5a7c1bea304ba6f
Lyudmila <milo4kamoya@gmail.com>
2022-09-24 18:44:51 -0700

    Reusing action types to get a token from Stripe API

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+//User data from Google
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+//Token from STRIPE API
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 }
