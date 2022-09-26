2058cc5f079f7d8d417715b9af696ff29fbf73cc
Lyudmila <milo4kamoya@gmail.com>
2022-09-26 12:26:38 -0700

    Adding Credits to a User

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,3 +1,4 @@
+const { serializeUser } = require('passport');
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
@@ -10,6 +11,9 @@ module.exports = app => {
       source: req.body.id
     })
 
-    console.log(charge)
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 }
