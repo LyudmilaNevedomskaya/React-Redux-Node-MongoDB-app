9110d1601fac27bea0bf36541d05885dc2c8a09f
Lyudmila <milo4kamoya@gmail.com>
2022-10-07 11:12:32 -0700

    Showing all entered data for review in FormReview component

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -7,6 +7,24 @@ const SurveyReview = (props) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      <div>
+        <div>
+          <label>Survey Title:</label>
+          <div>{props.formValues.title}</div>
+        </div>
+        <div>
+          <label>Subject Line:</label>
+          <div>{props.formValues.subject}</div>
+        </div>
+        <div>
+          <label>Email Body:</label>
+          <div>{props.formValues.body}</div>
+        </div>
+        <div>
+          <label>Recipient List:</label>
+          <div>{props.formValues.emails}</div>
+        </div>
+      </div>
       <button className="yellow darken-3 btn-flat" onClick={props.onCancel}>
         Back
       </button>
