68f52ec5cb881d136fc91739a598e87a927675c1
Lyudmila <milo4kamoya@gmail.com>
2022-10-07 11:23:09 -0700

    Finalizing Review Fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,13 +1,25 @@
 // SurveyReview shows users their form inputs for review
-
+import _ from 'lodash';
 import React from "react";
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyReview = (props) => {
+  const reviewFields = _.map(formFields, field => {
+    return (
+      <div key={field.name}>
+        <label>{field.label}</label>
+        <div>
+          {props.formValues[field.name]}
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={props.onCancel}>
         Back
       </button>
