b9da3bdd7fd1c7c506f72d83ada821a5748b2681
Lyudmila <milo4kamoya@gmail.com>
2022-10-07 10:31:09 -0700

    Navigating to SurveyFormReview after entering all data in the form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,22 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, {Component} from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = {showFormReview: false};
+
+  renderContent() {
+    if (this.state.showFormReview === true) {
+      return <SurveyReview />
+    }
+    return <SurveyForm onSurveySubmit={() => this.setState({showFormReview: true})}/>
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.renderContent()}
       </div>
     )
   }
